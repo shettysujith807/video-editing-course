f2e4245a6bf6dfc4064cd10a5567bdd0ca49f053
shettysujith807 <shettysujith807@gmail.com>
2024-12-23 15:46:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("enroll-button").addEventListener("click",function()
+{
+    alert("Thank you for your interest! Click'Buy Now'to secure your spot.");
+});
+document.getElementById("buy-now").addEventListener("click",function()
+{
+    alert("redirecting you to the payement page...");
+});
